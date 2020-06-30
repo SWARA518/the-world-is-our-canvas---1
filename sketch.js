222eee76ffcb2a407e0d10ec0ae1a7a89ac4f432
SWARA518 <63648720+SWARA518@users.noreply.github.com>
2020-06-30 16:18:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var canvas;
+
+function draw(){
+    createCanvas(500,500);
+    canvas = strokeWeight(4)
+
+}
+function mouseDragged(){
+    strokeWeight(18)
+}
+function mouseReleased(){
+    strokeWeight(0)
+}
